d5733690001483969a2fef9b506be42848eeb225
Jamal Hussain <66440388+HussainJam@users.noreply.github.com>
2023-09-13 08:48:54 -0700

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Navigation from "./Components/Navigation";
+import "./App.css";
+import HeroSection from "./Components/HeroSection";
+
+
+const App = () => {
+    return (
+      <>
+      <Navigation />
+      <HeroSection />
+      </>
+    )
+};
+
+export default App;
